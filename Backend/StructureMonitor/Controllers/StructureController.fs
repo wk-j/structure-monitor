63e9872170d3876143ff4803597b88b21819542a
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-09-04 12:41:33 +0700

    Created backend

---
+++ Backend/StructureMonitor/Controllers/StructureController.fs
@@ -0,0 +1,12 @@
+namespace StructureMonitor.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+
+
+[<Route("api/[controller]")>]
+type StructureController () =
+    inherit Controller()
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 ## Structure Monitor
+
+## Development
+
+```bash
+dotnet build Backend/StructureMonitor/StructureMonitor.fsproj
+dotnet run --project Backend/StructureMonitor/StructureMonitor.fsproj
+```
